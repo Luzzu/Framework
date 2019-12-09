532a6176946ce77dbc90d043eef103e81c4405ef
Ademar Crotti Junior <crottija@scss.tcd.ie>
2019-12-09 14:52:17 +0000

    updating hdt, jena and fuseki versions

--- luzzu-io/src/test/java/io/github/luzzu/io/impl/SPARQLEndPointProcessorTest.java
+++ luzzu-io/src/test/java/io/github/luzzu/io/impl/SPARQLEndPointProcessorTest.java
@@ -1,7 +1,6 @@
 package io.github.luzzu.io.impl;
 
-
-import org.apache.jena.fuseki.embedded.FusekiEmbeddedServer;
+import org.apache.jena.fuseki.embedded.FusekiServer;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.RDFDataMgr;
@@ -15,43 +14,41 @@ import org.junit.Test;
 
 import io.github.luzzu.exceptions.LuzzuIOException;
 import io.github.luzzu.io.IOProcessor;
-import io.github.luzzu.io.impl.SPARQLEndPointProcessor;
 import io.github.luzzu.semantics.commons.ResourceCommons;
 import io.github.luzzu.semantics.vocabularies.LMI;
 
 public class SPARQLEndPointProcessorTest extends Assert {
-	
+
 	private IOProcessor processor = null;
-	
+
 	private String datasetPLD = "http://example.org";
 	private String datasetLocation = this.getClass().getClassLoader().getResource("processor-datadumps/test-data-40377.nt").toExternalForm();
 	private Boolean generateReport = false;
 	private Model metricConf = ModelFactory.createDefaultModel();
-	private FusekiEmbeddedServer server;
-	
-	
+	private FusekiServer server;
+
 	@Before
 	public void setUp() throws Exception {
 		DatasetGraph dsg = TDBFactory.createDatasetGraph();
-		server = FusekiEmbeddedServer.make(9876, "/ds", dsg);
+		server = FusekiServer.make(9876, "/ds", dsg);
 		server.start();
 		Txn.executeWrite(dsg, () -> RDFDataMgr.read(dsg, datasetLocation));
-		
+
 		metricConf.add(metricConf.createStatement(ResourceCommons.generateURI(), LMI.metric, "io.github.luzzu.testing.metrics.SimpleCountMetric"));
-		processor = new SPARQLEndPointProcessor(datasetPLD, "http://localhost:9876/ds" , generateReport, metricConf);
+		processor = new SPARQLEndPointProcessor(datasetPLD, "http://localhost:9876/ds", generateReport, metricConf);
 	}
-	
+
 	@After
-	public void tearDown() throws Exception{
+	public void tearDown() throws Exception {
 		server.stop();
 	}
-	
+
 	@Test
 	public void setUpProcessTest() throws LuzzuIOException {
 		processor.setUpProcess();
 		assertEquals(1, processor.getNumberOfInitMetrics());
 	}
-	
+
 	@Test
 	public void processingTest() {
 		try {
@@ -60,7 +57,7 @@ public class SPARQLEndPointProcessorTest extends Assert {
 		} catch (LuzzuIOException | InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 		long triplesProcessed = 0;
 		try {
 			triplesProcessed = processor.getIOStats().get(0).getTriplesProcessed();
@@ -68,8 +65,8 @@ public class SPARQLEndPointProcessorTest extends Assert {
 			e.printStackTrace();
 		}
 		long expectedTriplesProcessed = 40377l;
-		
+
 		assertEquals(expectedTriplesProcessed, triplesProcessed);
 	}
-	
+
 }
